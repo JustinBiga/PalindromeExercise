3781bb9e07ad42ce2bbcc7797c56ee8cce926668
JustinBiga <BISENGOJ@GMAIL.COM>
2024-04-18 21:25:11 -0400

    init commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace PalindromeExercise
+{
+	public class WordSmith
+	{
+		public bool IsPalindrome(string word)
+		{
+			return word.SequenceEqual(word.Reverse());
+		}
+
+    }
+}
+
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,27 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+    { [Theory]
+        [InlineData("civic", true)]
+        [InlineData("deified", true)]
+        [InlineData("noon", true)]
+        [InlineData("repaper", true)]
+        [InlineData("check", false)]
+        [InlineData("madam", true)]
 
+        public void IsPalindromeTest(string word, bool expected)
+        {
+            var palindrome = new WordSmith();
+
+            var actual = palindrome.IsPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
+      
+       
     }
 }
